7d1a359394b2c71768a81950afb5003440dfe979
MipKip <83323382+MipKip@users.noreply.github.com>
2024-08-05 15:21:54 +0900

    new

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Kalnia, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const kalnia = Kalnia({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
